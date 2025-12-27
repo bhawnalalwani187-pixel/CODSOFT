a713c5a811c463bf67385c58bac5cc264f90af29
bhawna <bhawnalalwani187@gmail.com>
2025-12-27 15:10:24 +0530

    React app fixed

---
+++ codsoft/src/App.js
@@ -0,0 +1,13 @@
+import { memo } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>CodSoft Project</h1>
+      <p>My website is working</p>
+    </div>
+  );
+}
+
+export default App;
